d3cc9f4f993947c291dbbc5c75908f329b37ce0e
Mariano <mariano_farace@hotmail.com>
2021-10-28 18:19:59 +0200

    Funciona perfecto con UTF-8

--- test.js
+++ test.js
@@ -20,8 +20,8 @@ const encryptAndDelete = (path, encoding) => {
     let encrypted = cipher.update(buf, encoding, "hex")
     encrypted += cipher.final("hex")
 
-    fs.writeFileSync("encrypted-" + path, encrypted)
-    console.log('encrypted: ', encrypted) //TODO borrar
+    fs.writeFileSync("encrypted-" + path, encrypted) // Probablemente esto este causando problemas, tal vez tengas que agregar tostring(encode)
+    console.log('encrypted: ', encrypted)
         //TODO falta borre el archivo viejo
 
     return encrypted
@@ -29,14 +29,22 @@ const encryptAndDelete = (path, encoding) => {
 }
 
 
-const decryptData = (encrypted, originalEncoding) => {
-    let decipher = crypto.createDecipheriv(algorithm, key, iv)
+const decryptData = (path, originalEncoding) => { //Revisar que puede que esto de casualidad este pasando UTF necesario y no dependiente
+        //TODO el problema es que tenes que leer el archivo aca, pelotudo! Fijate que el siguiente comment no te joda la vida!
+        const content = fs.readFileSync(path, 'utf-8') //Ponerle await //TODO aca tiene que ser utf-8 original encoding!!!!!!!!!!!!!!!!!!!!!!!!
+        const buf = Buffer.from(content, 'hex');
 
-    let decrypted = decipher.update(encrypted, "hex", originalEncoding)
-    decrypted += decipher.final("utf-8") //TODO aca el utf lo tenes que cambiar por el argumento encoding????????
-    console.log('decrypted: ', decrypted) //TODO borar
 
-}
+        let decipher = crypto.createDecipheriv(algorithm, key, iv)
 
-let encrypted = encryptAndDelete("bananas.txt", "utf-8") //TODO aca el utf lo tenes que cambiar por el argumento encoding // aca lo vas a hacer async
-decryptData(encrypted, "utf-8") //TODO ponele un timeout para ver que pasa, y hace la otra asincrona
+        let decrypted = decipher.update(buf, "hex", originalEncoding)
+        decrypted += decipher.final(originalEncoding)
+        console.log('decrypted: ', decrypted)
+        fs.writeFileSync("decrypted-" + path, decrypted)
+
+    }
+    //TODO borar la variable encripted esta
+encryptAndDelete("bananas.txt", "utf-8") //TODO aca el utf lo tenes que cambiar por el argumento encoding // aca lo vas a hacer async
+decryptData("encrypted-bananas.txt", "utf-8") //TODO ponele un timeout para ver que pasa, y hace la otra asincrona. Le estas pasando un string no un archivo!
+
+//TODO borrar los console.log
